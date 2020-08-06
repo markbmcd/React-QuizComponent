80dc9cb35b403b041eec54f57946dc7e8c7aeb64
markbmcd <markbmcd@googlemail.com>
2020-08-06 18:27:12 +0100

    prop

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -7,12 +7,12 @@ render(){
 <main>
   <section>
     <p>
-      <p>{this.props.quiz_question.instruction_test}</p>
+      <p>{this.props.quiz_question.instruction_text}</p>
     </p>
   </section>
   <section className="buttons">
     <ul>
-      <Li>{this.props.quiz_question.answer.answer_options[0]} </Li>
+      <Li>{this.props.quiz_question.answer_options[0]} </Li>
 
     </ul>
   </section>
